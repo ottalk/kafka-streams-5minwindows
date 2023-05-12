1c92ab70edbf903e071cd5c2b4bc1ce0a02cdbac
Kuldip Ottal <kuldip.ottal@gmail.com>
2023-05-12 11:16:08 +0100

    updates for external config setup

--- src/main/java/com/harvicom/kafkastreams/Application.java
+++ src/main/java/com/harvicom/kafkastreams/Application.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 
+import com.harvicom.kafkastreams.processor.FiveMinWindowProperties;
+
+//import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableKafka
 @EnableKafkaStreams
+//@ConfigurationPropertiesScan("com.harvicom.kafkastreams")
+//@PropertySource("classpath:application.properties")
+@EnableConfigurationProperties(FiveMinWindowProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/harvicom/kafkastreams/processor/FiveMinWindowProperties.java
+++ src/main/java/com/harvicom/kafkastreams/processor/FiveMinWindowProperties.java
@@ -1,14 +1,18 @@
 package com.harvicom.kafkastreams.processor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@Profile("one")
-//@PropertySource("application.properties")
+@PropertySource("classpath:application.properties")
 @ConfigurationProperties(prefix = "fiveminwindows")
+//@ConfigurationPropertiesScan
 public class FiveMinWindowProperties {
+    @Value("${fiveminwindows.topicName}")
     private String topicName;
 
     public String getTopicName() {
