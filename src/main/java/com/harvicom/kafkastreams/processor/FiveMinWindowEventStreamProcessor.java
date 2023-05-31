fbd871ed3db7912816f6bafde75599fbcec5bf04
Kuldip Ottal <kuldip.ottal@gmail.com>
2023-05-31 23:03:00 +0100

    Working Unit test

--- src/main/java/com/harvicom/kafkastreams/processor/FiveMinWindowEventStreamProcessor.java
+++ src/main/java/com/harvicom/kafkastreams/processor/FiveMinWindowEventStreamProcessor.java
@@ -9,12 +9,7 @@ import org.apache.kafka.streams.kstream.Windowed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.text.DecimalFormat;
-
-import javax.annotation.PostConstruct;
-import javax.naming.spi.ObjectFactory;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.kafka.common.serialization.Serde;
@@ -32,28 +27,25 @@ import org.apache.kafka.streams.KeyValue;
 @Component
 public class FiveMinWindowEventStreamProcessor {
 
-    @Autowired
-    private StreamsBuilder streamsBuilder;
-
     @Value("${fiveminwindows.inputTopicName}")
-    String inputTopicName;
+    public String inputTopicName;
 
     @Value("${fiveminwindows.outputTopicName}")
-    String outputTopicName;
+    public String outputTopicName;
 
-    DecimalFormat df = new DecimalFormat("##########"); 
+    DecimalFormat df = new DecimalFormat("##########");
 
-    private String getNodeValue(ObjectNode node,String nodeValue) {
-        String result="";
-        if (node.has(nodeValue)){
-            result=node.get(nodeValue).asText();
+    private String getNodeValue(ObjectNode node, String nodeValue) {
+        String result = "";
+        if (node.has(nodeValue)) {
+            result = node.get(nodeValue).asText();
         }
         return result;
-    } 
+    }
 
-    private Double getNodeValueDouble(ObjectNode node,String nodeValue) {
-        Double result=0.0;
-        if (node.has(nodeValue)){
+    private Double getNodeValueDouble(ObjectNode node, String nodeValue) {
+        Double result = 0.0;
+        if (node.has(nodeValue)) {
             try {
                 return Double.parseDouble(node.get(nodeValue).asText());
             } catch (NumberFormatException nfe) {
@@ -62,10 +54,10 @@ public class FiveMinWindowEventStreamProcessor {
             }
         }
         return result;
-    } 
+    }
 
-    @PostConstruct
-    public void streamTopology() {
+    @Autowired
+    public void buildPipeline(StreamsBuilder streamsBuilder) {
 
         Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
         Serializer<JsonNode> jsonSerializer = new JsonSerializer();
@@ -73,30 +65,38 @@ public class FiveMinWindowEventStreamProcessor {
 
         StringSerializer stringSerializer = new StringSerializer();
         StringDeserializer stringDeserializer = new StringDeserializer();
-        Serde<String> stringSerde = Serdes.serdeFrom(stringSerializer,stringDeserializer);
+        Serde<String> stringSerde = Serdes.serdeFrom(stringSerializer, stringDeserializer);
 
         TimeWindowedSerializer<String> windowedSerializer = new TimeWindowedSerializer<>(stringSerializer);
         TimeWindowedDeserializer<String> windowedDeserializer = new TimeWindowedDeserializer<>();
-        Serde<Windowed<String>> windowedSerde = Serdes.serdeFrom(windowedSerializer,windowedDeserializer);
+        Serde<Windowed<String>> windowedSerde = Serdes.serdeFrom(windowedSerializer, windowedDeserializer);
 
-        //Consumed<String, JsonNode> consumerOptions = Consumed.with(Serdes.String(), jsonSerde).withTimestampExtractor(new StringTimestampExtractor());
-        //KStream<String, JsonNode> kStream = streamsBuilder.stream("streams-test-1", consumerOptions);
-        //KStream<String, JsonNode> kStream = streamsBuilder.stream(fiveMinWindowProperties.getTopicName(), Consumed.with(Serdes.String(), jsonSerde));
-        KStream<String, JsonNode> kStream = streamsBuilder.stream(inputTopicName, Consumed.with(Serdes.String(), jsonSerde));
+        // Consumed<String, JsonNode> consumerOptions = Consumed.with(Serdes.String(),
+        // jsonSerde).withTimestampExtractor(new StringTimestampExtractor());
+        // KStream<String, JsonNode> kStream = streamsBuilder.stream("streams-test-1",
+        // consumerOptions);
+        // KStream<String, JsonNode> kStream =
+        // streamsBuilder.stream(fiveMinWindowProperties.getTopicName(),
+        // Consumed.with(Serdes.String(), jsonSerde));
+        KStream<String, JsonNode> kStream = streamsBuilder.stream(inputTopicName,
+                Consumed.with(Serdes.String(), jsonSerde));
 
-
-
-        //kStream.filter((key, value) -> value.startsWith("Message_")).mapValues((k, v) -> v.toUpperCase()).peek((k, v) -> System.out.println("Key : " + k + " Value : " + v)).to("streams-test-2", Produced.with(Serdes.String(), Serdes.String()));
-        kStream.map(new KeyValueMapper<String,JsonNode,KeyValue<String,JsonNode>>() {
+        // kStream.filter((key, value) -> value.startsWith("Message_")).mapValues((k, v)
+        // -> v.toUpperCase()).peek((k, v) -> System.out.println("Key : " + k + " Value
+        // : " + v)).to("streams-test-2", Produced.with(Serdes.String(),
+        // Serdes.String()));
+        kStream.map(new KeyValueMapper<String, JsonNode, KeyValue<String, JsonNode>>() {
             @Override
             public KeyValue<String, JsonNode> apply(String key, JsonNode value) {
                 ObjectNode node = (ObjectNode) value;
-                //node.put("TRANSACTION_TIME",node.get("TRANSACTION_TIME").asText()+"TEST");
-                //String compositeKey=node.get("MERCHANT_NO").asText()+":"+node.get("DIV").asText()+":"+node.get("COUNTRY").asText();
-                String compositeKey=getNodeValue(node,"MERCHANT_NO")+getNodeValue(node,"DIV")+getNodeValue(node,"COUNTRY")+getNodeValue(node,"CRAP");
-                node.put("RESPONSE_TIME_CALC_DOUBLE",getNodeValueDouble(node, "RESPONSE_TIME")+5.5);
+                // node.put("TRANSACTION_TIME",node.get("TRANSACTION_TIME").asText()+"TEST");
+                // String
+                // compositeKey=node.get("MERCHANT_NO").asText()+":"+node.get("DIV").asText()+":"+node.get("COUNTRY").asText();
+                String compositeKey = getNodeValue(node, "MERCHANT_NO") + getNodeValue(node, "DIV")
+                        + getNodeValue(node, "COUNTRY") + getNodeValue(node, "CRAP");
+                node.put("RESPONSE_TIME_CALC_DOUBLE", getNodeValueDouble(node, "RESPONSE_TIME") + 5.5);
                 return new KeyValue<>(compositeKey, node);
             }
-        }).to(outputTopicName, Produced.with(Serdes.String(),jsonSerde));
+        }).to(outputTopicName, Produced.with(Serdes.String(), jsonSerde));
     }
 }
---
+++ src/test/java/com/harvicom/kafkastreams/ApplicationUnitTest.java
@@ -0,0 +1,88 @@
+package com.harvicom.kafkastreams;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Properties;
+
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import com.harvicom.kafkastreams.processor.FiveMinWindowEventStreamProcessor;
+
+class FiveMinWindowEventStreamProcessorUnitTest {
+
+    private FiveMinWindowEventStreamProcessor fiveMinWindowEventStreamProcessor;
+
+    Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+    Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+    Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+
+    StringSerializer stringSerializer = new StringSerializer();
+    StringDeserializer stringDeserializer = new StringDeserializer();
+    Serde<String> stringSerde = Serdes.serdeFrom(stringSerializer, stringDeserializer);
+
+    @BeforeEach
+    void setUp() {
+        fiveMinWindowEventStreamProcessor = new FiveMinWindowEventStreamProcessor();
+    }
+
+    @Test
+    void givenInputMessages_whenProcessed_thenWordCountIsProduced() {
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        fiveMinWindowEventStreamProcessor.inputTopicName="input-topic";
+        fiveMinWindowEventStreamProcessor.outputTopicName="output-topic";
+        fiveMinWindowEventStreamProcessor.buildPipeline(streamsBuilder);
+        Topology topology = streamsBuilder.build();
+
+        try (TopologyTestDriver topologyTestDriver = new TopologyTestDriver(topology, new Properties())) {
+
+            TestInputTopic<String, JsonNode> inputTopic = topologyTestDriver
+                .createInputTopic("input-topic", new StringSerializer(), new JsonSerializer());
+
+            TestOutputTopic<String, JsonNode> outputTopic = topologyTestDriver
+                .createOutputTopic("output-topic", new StringDeserializer(), new JsonDeserializer());
+
+
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode jsonNode = null;
+                try {
+                        jsonNode = mapper.readTree("{\"MERCHANT_NO\":\"12300002\",\"DIV\":\"0001\",\"COUNTRY\":\"UK\",\"RESPONSE_TIME\":\"0.95\"}");
+                } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                }    
+
+            inputTopic.pipeInput("key", jsonNode);
+
+            ObjectMapper mapper2 = new ObjectMapper();
+            JsonNode expectedJsonNode = null;
+
+            try {
+                expectedJsonNode = mapper2.readTree("{\"MERCHANT_NO\":\"12300002\",\"DIV\":\"0001\",\"COUNTRY\":\"UK\",\"RESPONSE_TIME\":\"0.95\",\"RESPONSE_TIME_CALC_DOUBLE\":6.45}");
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            } 
+
+            assertThat(outputTopic.readKeyValuesToList()).containsExactly(
+                    KeyValue.pair("123000020001UK", expectedJsonNode)
+                );
+        }
+    }
+
+}
